331f3ce1637365647ddca8d3275fb432e500b9ae
Penuel <panuelpenuel7@gmail.com>
2023-04-24 14:36:31 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+        let x=1//this is a number
+        let y=2//this a number
+        let name='penuel'//this is a string
+        let age=24
+        let isold=age>20//This is a bolean
+        let showAlert=function()
+        {
+        let x=1//this is a number
+        let y=2//this a number
+        let name='penuel '//this is a string
+        let age=24
+        let isold=age>20//This is a bolean
+        alert('Hello, welcome ' + name + age)
+        }
+        let getName=function(){
+                prompt('What is your name?')
+        }
